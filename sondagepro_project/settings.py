66bf0183893693004983ef855138d3fee7f41f63
NickHGA <primelhouaga22@gmail.com>
2025-07-16 12:11:53 +0100

    Initial commit

--- sondagepro_project/settings.py
+++ sondagepro_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,11 +75,13 @@ WSGI_APPLICATION = 'sondagepro_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',  # fallback
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
